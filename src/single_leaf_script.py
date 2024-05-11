7937b34b913884a86bcd01915a3eb1f145bd7353
Novo <eunovo9@gmail.com>
2024-05-11 13:02:51 +0100

    chore: Update print statement in single_leaf_script

--- src/single_leaf_script.py
+++ src/single_leaf_script.py
@@ -1,7 +1,7 @@
 from secp256k1 import *
 
 def single_leaf_script():
-  print("Running Taproot test")
+  print("Running Single Leaf test")
   internal_key = "a34b99f22c790c4e36b2b3c2c35a36db06226e41c692fc82b8b56ac1c540c5bd"
   internal_key_bytes = bytes.fromhex(internal_key)
   leaf_version = "cc"
